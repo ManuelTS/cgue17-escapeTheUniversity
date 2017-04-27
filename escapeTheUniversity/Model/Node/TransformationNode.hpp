25c433bf4ea8d3bcf625aa450792b988c1447a46
Manuel T. Schrempf <manuel.t.schrempf@gmail.com>
2017-04-27 18:46:40 +0200

    Small soundManager volume overhaul.

--- escapeTheUniversity/Model/Node/TransformationNode.hpp
+++ escapeTheUniversity/Model/Node/TransformationNode.hpp
@@ -5,7 +5,7 @@ class TransformationNode : public Node
 {
 private:
 	const float ANGLE = 0.010f;
-	const float MAX_ROTATION = 0.0070f;
+	const float MAX_ROTATION = 70.0f/1000.0f;
 	float currentRotation = 0.0f;
 	bool transform = false;
 public:
